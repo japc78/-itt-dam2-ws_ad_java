47da8efcf174090e04edcfc1e0ab957bd0caf2da
japc <japc.grafico@gmail.com>
2020-01-22 02:31:29 +0100

    Entrega del ejercicio.

---
+++ ActividadUF2-1/readme.md
@@ -0,0 +1,21 @@
+# Actividad UF1-2. Manejo de conectores a BBDD. Gestión del almacen. 
+Las clases realizadas se encuentran comentadas paso por paso. Algunos comentarios contienen la etiqueta `STUDY`, la cual utilizo junto con la extensión **To do Tree** de VS Code, para marcar donde debo repasar y estudiar, y asi acceder de una forma clara y directa en una lista de acciones que crea la extensión.
+
+La tecnología utilizada `Java` con las clases,  `sql.Connection`, `sql.DriverManager`, `sql.PreparedStatement`, `sql.SQLException`, `util.InputMismatchException`, `util.Scanner`.
+
+Capturas de resultados:
+
+![](/assets/img-002.png)
+![](/assets/img-003.png)
+![](/assets/img-004.png)
+![](/assets/img-005.png)
+![](/assets/img-006.png)
+
+Como herramienta IDE/Editor **Vs Code**, con las siguientes extensiones:
+- [Java Extension Pack](https://marketplace.visualstudio.com/items?itemName=vscjava.vscode-java-pack).
+- [Todo Tree](https://marketplace.visualstudio.com/items?itemName=Gruntfuggly.todo-tree)
+- [Markdown All in One](https://marketplace.visualstudio.com/items?itemName=yzhang.markdown-all-in-one).
+
+Juan Antonio Pavón Carmona.  
+Programación de servicios y procesos.  
+ITT DAM 2.
--- ActividadUF2-1/src/Principal.java
+++ ActividadUF2-1/src/Principal.java
@@ -1,51 +1,58 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import com.mysql.jdbc.PreparedStatement;
-
 public class Principal {
+	// Validacion: Codigo producto [0], precio[1], stock[2]
+	private static final String[] pattern = new String[]{"[a-zA-Z]{3}\\d", "^\\d+(\\.\\d+)?$", "^\\d+"};
 	private static Scanner lector;
 
 	public static void main(String[] args) {
-		Connection con = abrirConexionBD();
-		if (con == null)
+
+		Connection conexion = abrirConexionBD();
+		if (conexion == null)
 			return;
 		lector = new Scanner(System.in);
 		int opc;
 		do {
 			mostrarMenu();
-			opc = lector.nextInt();
-			lector.nextLine(); // Recogemos el retorno de carro.
-			System.out.println();
-			/*
-			 * Añade aquí una estructura switch que permita llamar a un método u otro según
-			 * el valor de la variable opc.
-			 */
 
-			switch (opc) {
-			case 1:
-				listarProductos(con);
-				break;
-			case 2:
-				nuevoProducto(con);
-				break;
-			case 3:
-				actualizarStock(con);
-				break;
-			case 4:
-				System.out.println("Programa finalizado");
-				break;
+			try {
+				opc = lector.nextInt();
+				lector.nextLine(); // Recogemos el retorno de carro.
+				System.out.println();
 
-			default:
-				System.out.println("Opción incorrecta");
-				break;
+				switch (opc) {
+					case 1:
+						listarProductos(conexion);
+						break;
+					case 2:
+						nuevoProducto(conexion);
+						break;
+					case 3:
+						actualizarStock(conexion);
+						break;
+					case 4:
+						System.out.println("Programa finalizado");
+						break;
+
+					default:
+						System.out.println("Opción incorrecta");
+						break;
+				}
+
+			} catch (InputMismatchException e) {
+				System.out.println("Debe de ser un valor del menu del 1 al 4");
+				lector.nextLine();
+				opc = 0;
 			}
 
 		} while (opc != 4);
-		cerrarConexion(con); // Pasamos como argumento la conexión a cerrar.
+		cerrarConexion(conexion); // Pasamos como argumento la conexión a cerrar.
 		lector.close();
 	}
 
@@ -87,19 +94,14 @@ public class Principal {
 		return conexion;
 	}
 
-	private static void cerrarConexion(Connection con) {
-		/*
-		 * Añade el código necesario para cerrar la conexión que
-		 * recibe esté método como argumento.
-		 */
+	private static void cerrarConexion(Connection conexion) {
 		try {
-			con.close();
+			conexion.close();
 			System.out.println("App: conexion BD cerrada");
 		} catch (SQLException e) {
 			System.out.println("ERROR: al cerrar conexión con la BD");
 			e.printStackTrace();
 		}
-
 	}
 
 	public static void mostrarMenu() {
@@ -112,7 +114,6 @@ public class Principal {
 		System.out.println("4. Terminar programa");
 		System.out.println("--------------------------------------");
 		System.out.println("¿Qué opción eliges?");
-
 	}
 
 	private static void listarProductos(Connection conexion) {
@@ -122,7 +123,7 @@ public class Principal {
 			String sql = "SELECT * FROM PRODUCTO ORDER BY ?";
 			String att = "DESCRIPCION";
 			PreparedStatement s;
-			s = (PreparedStatement) conexion.prepareStatement(sql);
+			s = conexion.prepareStatement(sql);
 			s.setString(1, att);
 
 			// Ejecuta la sentencia
@@ -157,18 +158,148 @@ public class Principal {
 		}
 	}
 
-	private static void nuevoProducto(Connection con) {
-		/*
-		 * Añade el código necesario para añadir un nuevo producto.
-		 */
+	private static void nuevoProducto(Connection conexion) {
+		String[] temp = new String[5];
 
+		// Se recogen los valores del nuevo producto.
+
+		// Codigo articulo.
+		System.out.println("Código del articulo. Del tipo XXX1(3 letras y un número)");
+		do {
+			temp[0] = lector.nextLine();
+			if (!temp[0].matches(pattern[0]))
+				System.out.println("Codigo incorrecto, vuele a intentarlo");
+
+			else if (exists(conexion, temp[0]))
+				System.out.println("El codigo ya existe en la Bd, vuelve a intentarlo");
+
+		} while ((!temp[0].matches(pattern[0]) || (exists(conexion, temp[0]))));
+
+		// Descripcion articulo
+		System.out.println("Descripcion del articulo:");
+		temp[1] = lector.nextLine();
+
+		// Precio
+		System.out.println("Introduce un precio");
+		do {
+			temp[2] = lector.nextLine();
+			if (!temp[2].matches(pattern[1]))
+				System.out.println("Precio incorrecto, vuele a intentarlo");
+		} while (!temp[2].matches(pattern[1]));
+
+		// Stock
+		System.out.println("Introduce el stock");
+		do {
+			temp[3] = lector.nextLine();
+			if (!temp[3].matches(pattern[2]))
+				System.out.println("Stock incorrecto, vuele a intentarlo");
+		} while (!temp[3].matches(pattern[2]));
+
+		// Stock minimo
+		System.out.println("Introduce el stock minimo");
+		do {
+			temp[4] = lector.nextLine();
+			if (!temp[4].matches(pattern[2]))
+				System.out.println("Stock minimo incorrecto, vuele a intentarlo");
+		} while (!temp[4].matches(pattern[2]));
+
+		// SQL add producto.
+		String sql = "INSERT INTO PRODUCTO (CODIGO, DESCRIPCION, PRECIO, STOCK, MINIMO) " +
+				"VALUES (?, ?, ?, ?, ?)";
+
+		//STUDY PrepareStatement -> Manera segura de preparar la sentencia SQL.
+		PreparedStatement s;
+		try {
+			// Se envia la SQL sin parametros
+			s = conexion.prepareStatement(sql);
+
+			// Se envian los parametros
+			for (int i = 0; i < temp.length; i++) {
+				s.setString(i+1, temp[i]);
+			}
+
+			// Se ejecuta la sentencia y se recoge el numero de registros afectados.
+			int updates = s.executeUpdate();
+			System.out.println("Sentenecia SQL ejecutada con exito");
+			System.out.println("Registros afectados: " + updates);
+
+		} catch (SQLException e) {
+			System.out.println("Error: al añadir el articulo en la BD.");
+			e.printStackTrace();
+		}
 	}
 
-	private static void actualizarStock(Connection con) {
-		/*
-		 * Añade el código necesario para actualizar el stock.
-		 */
+	private static void actualizarStock(Connection conexion) {
 
+		String[] temp = new String[2];
+
+		// Se recoge codigo de producto y stock a reponer.
+		// Codigo
+		System.out.println("Código del articulo. Del tipo XXX1(3 letras y un número)");
+		do {
+			temp[0] = lector.nextLine();
+			if (!temp[0].matches(pattern[0]))
+				System.out.println("Codigo incorrecto, vuele a intentarlo");
+		} while (!temp[0].matches(pattern[0]));
+
+		// Stock
+		System.out.println("¿Cuantas unidades quieres reponer?");
+		do {
+			temp[1] = lector.nextLine();
+			if (!temp[1].matches(pattern[2]))
+				System.out.println("Stock incorrecto, vuele a intentarlo");
+		} while (!temp[1].matches(pattern[2]));
+
+		// Se comprueba si existe el codigo de producto en la BD para realizar la actualizacion del stock.
+		if (exists(conexion, temp[0])) {
+			try {
+				// Se crea la sentencia
+				String sql = "UPDATE PRODUCTO SET STOCK = STOCK + ? WHERE CODIGO = ?";
+
+				// Se pasa la sentencia SQL
+				PreparedStatement s = conexion.prepareStatement(sql);
+
+				// Se pasan los parametros
+				s.setString(1, temp[1]); // Stock
+				s.setString(2, temp[0]); // Codigo Producto
+
+				// Se ejecuta la actualizacion.
+				s.executeUpdate();
+
+				System.out.println("Stock actualizado correctamente");
+
+			} catch (SQLException e) {
+				System.out.println("Error: al actualizar el stock del producto");
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("El producto introducido no existe");
+			System.out.println("No es posible actualizar el stock");
+		}
 	}
 
+	/**
+	 * Comprueba si el producto exite o no.
+	 * @param conexion Se le pasa la conexion a la BD.
+	 * @param codigo Codigo del producto
+	 * @return Retorna True si el producto existe, False si el producto no existe y Null si ha habido algun error de conexion.
+	 */
+	private static Boolean exists (Connection conexion, String codigo) {
+		Boolean b = null;
+		try {
+			String sql = "SELECT * FROM PRODUCTO WHERE CODIGO = ?";
+			PreparedStatement s;
+			s = conexion.prepareStatement(sql);
+			s.setString(1, codigo);
+
+			// Ejecuta la sentencia
+			ResultSet rs = s.executeQuery();
+			b = (rs.first())?true : false;
+
+		} catch (SQLException e) {
+			System.out.println("Error: no ha sido posible comprobar si el producto exite");
+			e.printStackTrace();
+		}
+		return b;
+	}
 }
